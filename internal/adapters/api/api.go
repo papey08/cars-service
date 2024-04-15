7e36ca0e46de03a5891a6f51d6e9df3f94255c22
papey08 <popov.m4tvei@yandex.ru>
2024-04-15 18:26:59 +0300

    implement api interface

---
+++ internal/adapters/api/api.go
@@ -0,0 +1,71 @@
+package api
+
+import (
+	"cars-service/internal/model"
+	"context"
+	"encoding/json"
+	"errors"
+	"io"
+	"net/http"
+	"net/url"
+)
+
+type apiImpl struct {
+	url string
+	http.Client
+}
+
+func (a *apiImpl) GetInfo(ctx context.Context, regNum string) (model.Car, error) {
+	params := url.Values{}
+	params.Add("regNum", regNum)
+	reqUrl := a.url + "?" + params.Encode()
+
+	req, _ := http.NewRequestWithContext(ctx, http.MethodGet, reqUrl, nil)
+	resp, err := a.Client.Do(req)
+	if err != nil {
+		return model.Car{}, errors.Join(model.ErrApiError, err)
+	}
+	defer func() {
+		_ = resp.Body.Close()
+	}()
+	if resp.StatusCode != http.StatusOK {
+		return model.Car{}, model.ErrApiError
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return model.Car{}, errors.Join(model.ErrApiError, err)
+	}
+
+	var data carInfo
+	if err = json.Unmarshal(body, &data); err != nil {
+		return model.Car{}, errors.Join(model.ErrApiError, err)
+	}
+
+	return model.Car{
+		RegNum: data.RegNum,
+		Mark:   data.Mark,
+		Model:  data.Model,
+		Year:   data.Year,
+		Owner: model.Owner{
+			Name:       data.Owner.Name,
+			Surname:    data.Owner.Surname,
+			Patronymic: data.Owner.Patronymic,
+		},
+	}, nil
+
+}
+
+type carInfo struct {
+	RegNum string    `json:"regNum"`
+	Mark   string    `json:"mark"`
+	Model  string    `json:"model"`
+	Year   int       `json:"year"`
+	Owner  ownerInfo `json:"owner"`
+}
+
+type ownerInfo struct {
+	Name       string `json:"name"`
+	Surname    string `json:"surname"`
+	Patronymic string `json:"patronymic"`
+}
--- internal/adapters/api/api_interface.go
+++ internal/adapters/api/api_interface.go
@@ -3,8 +3,16 @@ package api
 import (
 	"cars-service/internal/model"
 	"context"
+	"net/http"
 )
 
 type Api interface {
 	GetInfo(ctx context.Context, regNum string) (model.Car, error)
+}
+
+func NewApi(url string) Api {
+	return &apiImpl{
+		url:    url,
+		Client: http.Client{},
+	}
 }
